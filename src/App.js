cddcce9c566b6d6f404437f320fdcc4002577a23
Vasile Midrigan <111rigan111@gmail.com>
2023-10-18 11:22:05 +0100

    Adding User Pages

--- src/App.js
+++ src/App.js
@@ -12,6 +12,8 @@ import { PostsList } from './features/posts/PostsList'
 import { AddPostForm } from './features/posts/AddPostForm'
 import { SinglePostPage } from './features/posts/SinglePostPage'
 import { EditPostForm } from './features/posts/EditPostForm'
+import { UsersList } from './features/users/UsersList'
+import { UserPage } from './features/users/UserPage'
 
 import TotalLikes from './features/likes/totalLikes'
 
@@ -34,6 +36,8 @@ function App() {
           />
           <Route exact path="/posts/:postId" component={SinglePostPage} />
           <Route exact path="/editPost/:postId" component={EditPostForm} />
+          <Route exact path="/users" component={UsersList} />
+          <Route exact path="/users/:userId" component={UserPage} />
           <Redirect to="/" />
         </Switch>
       </div>
--- src/features/users/usersSlice.jsx
+++ src/features/users/usersSlice.jsx
@@ -20,3 +20,8 @@ export const fetchUsers = createAsyncThunk('users/fetchUsers', async () => {
 })
 
 export default usersSlice.reducer
+
+export const selectAllUsers = (state) => state.users
+
+export const selectUserById = (state, userId) =>
+  state.users.find((user) => user.id === userId)
